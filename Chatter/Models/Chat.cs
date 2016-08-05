34e9ef6d70377f3cf7d17d943ce872df79002550
Malyssa Winters <malyssawinters5@gmail.com>
2016-08-05 11:02:55 -0400

    initial commit

---
+++ Chatter/Models/Chat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Chatter.Models
+{
+    public class Chat
+    {
+        //properties
+        [Key]
+        public int ChatID { get; set; }
+        [MaxLength (149)]
+        public string Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        //navigation properties
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
